41594a7170322d21de93afdcb9523a6fc69730ab
ankit dixit <ankitdixit.nitb@gmail.com>
2019-01-10 14:15:50 +0530

    Component

---
+++ src/Components/Tutorial/TutApp.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Person from './Person/Person';
+
+class TutApp extends Component {
+  
+    state = {
+        persons: [
+          { name: 'Max', age: 28 },
+          { name: 'Manu', age: 29 },
+          { name: 'Stephanie', age: 26 }
+        ],
+        otherState: 'some other value'
+      }
+    
+      switchNameHandler = (newName) => {
+        // console.log('Was clicked!');
+        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+        this.setState( {
+          persons: [
+            { name: newName, age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephanie', age: 27 }
+          ]
+        } )
+      }
+    
+      nameChangedHandler = (event) => {
+        this.setState( {
+          persons: [
+            { name: 'Max', age: 28 },
+            { name: event.target.value, age: 29 },
+            { name: 'Stephanie', age: 26 }
+          ]
+        } )
+      }
+      
+  render() {
+    return (
+      <div className="TutApp">
+       <p>Hii you inside th Tut App</p>
+       <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+
+      </div>
+    )
+  }
+}
+export default TutApp;
